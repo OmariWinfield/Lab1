97990e674a81039370b1eee21b3dd5b17e5ecdd8
Omari Winfield <Omari.Winfield@cix.csi.cuny.edu>
2026-02-03 13:09:30 -0500

    Add FindWinner and PrintTable functions
    Added functions to find the winner and print a table.

--- Lab1/Header.h
+++ Lab1/Header.h
@@ -1,5 +1,19 @@
 #pragma once
-struct Programmer {
+struct Programmer 
+{
 	int codeLines;
 	int Comments;
 };
+
+void FindWinner(Programmer Programmers[], int index) 
+{
+	while(Programmers[index].codeLines > 1000)
+	{
+		
+	}
+}
+
+void PrintTable(int data) {
+	cout << "Programmer\tLines of Codes\tLines of Comments\n";
+	
+}
