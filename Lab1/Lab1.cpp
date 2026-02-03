e67ade2359274e83bb4f0bbef0a1a3a40cb2722b
Omari Winfield <Omari.Winfield@cix.csi.cuny.edu>
2026-02-03 12:39:35 -0500

    Modify main function to include file reading logic

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "Header.h"
 #include <fstream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Programmer Progress[10];
+	while (Progress.codeLines > 1000)
+	{
+		ifstream file("program11.txt");
+		Progress
+	}
 }
 
