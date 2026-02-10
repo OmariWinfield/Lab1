9b2fe874c893d2c2a25946ea4a69aeda38582baf
Omari Winfield <Omari.Winfield@cix.csi.cuny.edu>
2026-02-10 01:42:06 -0500

    Refactor Header.h with new function signatures

--- Lab1/Header.h
+++ Lab1/Header.h
@@ -1,32 +1,14 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 struct Programmer 
 {
 	int codeLines;
 	int Comments;
 };
 
-void FindWinner(Programmer Programmers[], int index) 
-{
-	while(Programmers[index].codeLines > 1000)
-	{
-		
-	}
-}
-
-void PrintTable(int data) {
-	cout << "Programmer\tLines of Codes\tLines of Comments\n";
-	
-}
-/*// programmer.h
-// Header file containing the Programmer structure definition
-
-#ifndef PROGRAMMER_H
-#define PROGRAMMER_H
-
-struct Programmer {
-    int linesOfCode;
-    int linesOfComments;
-};
-
-#endif
-*/
+void FindWinner(Programmer Programmers[], int& winner);
+void PrintTable(Programmer Programmers[], int winner);
+void InitializeArray(Programmer Programmers[]);
+int FindSmallestK(Programmer Programmers[], int winner);
